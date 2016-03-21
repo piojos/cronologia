73db0d1b67076c9c03db4f67d7f14c1829db7074
raidho-mx <miranda@raidho.mx>
2016-03-21 00:58:18 -0600

    Static index: :shower: intro done

--- sultana/index.php
+++ sultana/index.php
@@ -6,11 +6,83 @@
 
 	get_header(); ?>
 
-	<div>
-		<h1>Cronología de Diseño Gráfico en Monterrey</h1>
-	</div>
+	<section id="mainTitle">
 
-	<section class="year latest">
+		<div class="detailLine top"></div>
+
+		<div class="contain">
+			<div class="slim left">
+				<h1 class="logo">
+					Cronología
+					<span>del</span>
+					diseño gráfico
+					<span>en</span>
+					Monterrey
+				</h1>
+			</div>
+			<div id="timelineAbout" class="slim left">
+				<p>Un recuento y repositorio de la historia de la ciudad, su cultura y sus empresas a través del diseño gráfico.</p>
+
+				<p>Estás consultando el primer metalíbro por Raidho y curado Oscar Estrada.</p>
+			</div>
+		</div>
+
+	</section>
+
+
+
+
+
+
+
+	<section id="prologue">
+
+		<div class="detailLine"></div>
+
+		<div class="contain">
+			<div class="slim left sofia">
+				1902 – 2015
+			</div>
+			<div class="wide right">
+				I. <strong>Prólogo</strong>
+			</div>
+
+			<div class="slim left image">
+				<a herf="http://cronologia.mx/wp-content/uploads/2015/11/Screen-Shot-2015-06-05-at-10.20.43.png" target="_blank">
+					<img src="http://cronologia.mx/wp-content/uploads/2015/11/Screen-Shot-2015-06-05-at-10.20.43-300x169.png" alt="pulpo rosa en fondo azul">
+				</a>
+				<p class="caption">Se me hace importante destacar que nada hacia el noroeste.</p>
+
+				<a herf="http://cronologia.mx/wp-content/uploads/2015/11/Screen-Shot-2015-06-05-at-10.18.38.png" target="_blank">
+					<img src="http://cronologia.mx/wp-content/uploads/2015/11/Screen-Shot-2015-06-05-at-10.18.38-300x169.png" alt="">
+				</a>
+				<p class="caption">Aquí anda viendo la chiquía.</p>
+			</div>
+
+			<div class="wide right">
+				<h2>Antigua Gráfica</h2>
+
+				<p>Steven Heller es uno de los mejores escritores dedicados al diseño gráfico que conozco, su aportación al conocimiento general que rodea la gráfica global es incomparable y punto de referencia para millones de diseñadores, incluyéndome.</p>
+
+				<p>Todos los días envía un newsletter de parte de su sección especial de Print Magazine que se llama <a href="http://www.printmag.com/daily-heller/" target="_blank">‘The Daily Heller’</a> de la cual se tienen que inscribir en este momento si les interesa saber más sobre las raíces históricas del diseño gráfico mundial.</p>
+			</div>
+		</div>
+
+	</section>
+
+
+
+
+
+
+
+<div id="timeline">
+
+
+	<section class="latest">
+
+		<div class="newPostLine"></div>
+
 		<div class="number">Nuevo</div>
 		<ul class="projects">
 			<li>
@@ -19,7 +91,52 @@
 				</a>
 			</li>
 		</ul>
+
 	</section>
+
+
+
+
+	<section id="newChapter">
+
+		<div class="number range">
+			1988<br>-<br>2001
+			<a class="close">×</a>
+		</div>
+
+		<div class="title">
+			<div class="wide left sofia">
+				<h2>Epígonos</2>
+				<p class="author">Por Oscar Estrada</p>
+			</div>
+			<div class="slim right sofia">
+				Poco antes de 1900 a 1950.
+			</div>
+		</div>
+
+
+		<div class="content show">
+			<div class="slim left">
+				<h2>Antigua Gráfica</h2>
+
+				<p>Steven Heller es uno de los mejores escritores dedicados al diseño gráfico que conozco, su aportación al conocimiento general que rodea la gráfica global es incomparable y punto de referencia para millones de diseñadores, incluyéndome.</p>
+
+				<p>Todos los días envía un newsletter de parte de su sección especial de Print Magazine que se llama <a href="http://www.printmag.com/daily-heller/" target="_blank">‘The Daily Heller’</a> de la cual se tienen que inscribir en este momento si les interesa saber más sobre las raíces históricas del diseño gráfico mundial.</p>
+			</div>
+			<div class="wide right image">
+				<a herf="http://cronologia.mx/wp-content/uploads/2015/11/Screen-Shot-2015-06-05-at-10.20.43.png" target="_blank">
+					<img src="http://cronologia.mx/wp-content/uploads/2015/11/Screen-Shot-2015-06-05-at-10.20.43-300x169.png" alt="pulpo rosa en fondo azul">
+				</a>
+				<p class="caption">Se me hace importante destacar que nada hacia el noroeste.</p>
+
+			</div>
+		</div>
+
+	</section>
+
+
+
+
 	<section class="year">
 		<div class="number">1988</div>
 		<ul class="projects">
@@ -54,15 +171,27 @@
 	</section>
 	<section class="year">
 		<div class="number">2010</div>
-		<ul class="projects">
+		<ul class="articles">
 			<li class="article">
-				<a href="http://cronologia.mx/las-orquestas-y-el-jazz/" class="sofia">2010: Las Orquestas y el Jazz</a>
+				<a href="http://cronologia.mx/las-orquestas-y-el-jazz/" class="sofia">
+					Las Orquestas y el Jazz <span>por Gerardo Ortíz</span>
+				</a>
 			</li>
+			<li class="article">
+				<a href="http://cronologia.mx/el-manifiesto-supermodernista/" class="sofia">
+					Supermodernismo <span>por Mircea Turcan</span>
+				</a>
+			</li>
+		</ul>
+		<ul class="projects">
 			<li>
 				<a href="http://cronologia.mx/mitosis-010/" class="sofia">2010: Mitosis 010</a>
 			</li>
-			<li class="article">
-				<a href="http://cronologia.mx/el-manifiesto-supermodernista/" class="sofia">2010: Supermodernismo</a>
+			<li>
+				<a href="http://cronologia.mx/kinky/" class="sofia">2001: Kinky</a>
+			</li>
+			<li>
+				<a href="http://cronologia.mx/post-mas-nuevisimo/" class="sofia">2015: Post más nuevísimo</a>
 			</li>
 		</ul>
 	</section><?php
