85c0565354dfc0e24768591d6ac10edc668f7a0e
Daniel Miranda <mirand@raidho.mx>
2016-03-27 21:52:18 -0600

    Updated index

--- sultana/index.php
+++ sultana/index.php
@@ -4,217 +4,72 @@
 	 *	Index
 	 */
 
-	get_header(); ?>
+	get_header(); /*
+	if(have_posts()) :
+	echo '<ul>';
+		while(have_posts()) : the_post(); ?>
+			<li><a href="<?php the_permalink(); ?>"><strong class="sofia"><?php the_field('year'); ?></strong> <?php the_title(); ?></a></li><?php
+		endwhile;
+	echo '</ul>';
+endif; */
 
-	<section id="mainTitle">
+// get posts
+$posts = get_posts(array(
+	'posts_per_page'	=> -1,
+	'meta_key'			=> 'year',
+	'orderby'			=> 'meta_value_num',
+	'order'				=> 'ASC'
+));
 
-		<div id="Francesca" class="detailLine top"></div>
+	if( $posts ):
 
-		<div class="contain">
-			<div class="slim left">
-				<h1 class="logo">
-					Cronología
-					<span>del</span>
-					diseño gráfico
-					<span>en</span>
-					Monterrey
-				</h1>
-			</div>
-			<div id="timelineAbout" class="slim left">
-				<p>Un recuento y repositorio de la historia de la ciudad, su cultura y sus empresas a través del diseño gráfico.</p>
+		foreach( $posts as $post ):
+			setup_postdata( $post );
 
-				<p>Estás consultando el primer metalíbro por Raidho y curado Oscar Estrada.</p>
-			</div>
-		</div>
+		$annoNuevo = get_field('year');
 
-	</section>
+			if($anno == '') { // Post más reciente.
+				if (in_category('2')) { // Es Artículo
+					echo '<section class="year latest"><div class="number">Artículo</div><ul class="projects">';
+				} else { // Es Post
+					echo '<section class="year latest"><div class="number">Nuevo</div><ul class="projects">';
+				}
+				// echo '<section class="year latest"><div class="number">Latest</div><ul class="projects">';
+			} elseif($annoNuevo == $anno) {} else {
+				echo '</section><section class="year"><div class="number">'.$annoNuevo.'</div><ul class="projects">';
+			}
 
-
-
-
-
-
-
-	<section id="prologue">
-
-		<div class="detailLine"></div>
-
-		<div class="contain">
-			<div class="smallLogo">
-				O I O
-			</div>
-			<div class="slim left sofia">
-				1902 – 2015
-			</div>
-			<div class="wide right sofia" style="margin-bottom:6em">
-				I. <strong>Prólogo</strong>
-			</div>
-
-			<article>
-				<div class="slim left image">
-					<a herf="http://cronologia.mx/wp-content/uploads/2015/11/Screen-Shot-2015-06-05-at-10.20.43.png" target="_blank">
-						<img src="http://cronologia.mx/wp-content/uploads/2015/11/Screen-Shot-2015-06-05-at-10.20.43-300x169.png" alt="pulpo rosa en fondo azul">
-					</a>
-					<p class="caption">Se me hace importante destacar que nada hacia el noroeste.</p>
-				</div>
-
-				<div class="wide right">
-					<h2>Antigua Gráfica</h2>
-
-					<p>Steven Heller es uno de los mejores escritores dedicados al diseño gráfico que conozco, su aportación al conocimiento general que rodea la gráfica global es incomparable y punto de referencia para millones de diseñadores, incluyéndome.</p>
-
-					<p>Todos los días envía un newsletter de parte de su sección especial de Print Magazine que se llama <a href="http://www.printmag.com/daily-heller/" target="_blank">‘The Daily Heller’</a> de la cual se tienen que inscribir en este momento si les interesa saber más sobre las raíces históricas del diseño gráfico mundial.</p>
-				</div>
-			</article>
-		</div>
-
-	</section>
+			$cat = get_the_category(); ?>
 
 
+			<li<?php if (in_category('2')) echo ' class="article"'; ?>>
+				<a href="<?php the_permalink(); ?>" class="sofia"><?php
+
+				echo $annoNuevo.': ';
+				// Tester
+				// if($anno == '') {
+				// 	echo '<span>Nuevo</span>';
+				// } elseif($annoNuevo == $anno) {
+				// 	echo '<span>Mismo</span>';
+				// } else {
+				// 	echo '<span>Otro</span>';
+				// }
+
+				the_title(); ?></a>
+			</li><?php
 
 
+		$anno = get_field('year');
 
+			if($anno == '') {
+				echo '</ul></section>';
+			} elseif($annoNuevo == $anno) {} else {
+				echo '</ul></section>';
+			}
 
+		endforeach;
+		wp_reset_postdata();
 
-<div id="timeline">
-
-
-	<section id="latestPost">
-		<div class="newPostLine"><span>Nuevo</span></div>
-
-		<div class="contain">
-			<a href="http://cronologia.mx/los-pioneros/" class="sofia project">
-				<h3>Los Pioneros</h3>
-				<p class="kepler">1900 por <span>Autor Apellido</span></p>
-			</a>
-		</div>
-	</section>
-
-
-
-
-	<section id="newChapter">
-
-		<div class="period range">
-			<span>1988<br>-<br>2001</span>
-			<button class="close">×</button>
-		</div>
-		<hr>
-		<div class="title">
-			<div class="contain">
-				<div class="wide left sofia">
-					<h2>Epígonos</h2>
-					<p class="author">Por Oscar Estrada</p>
-				</div>
-				<div class="slim right sofia">
-					<p>Poco antes de 1900 a 1950.</p>
-				</div>
-
-				<article class="show">
-					<div class="wide left">
-						<p>Steven Heller es uno de los mejores escritores dedicados al diseño gráfico que conozco, su aportación al conocimiento general que rodea la gráfica global es incomparable y punto de referencia para millones de diseñadores, incluyéndome.</p>
-
-						<p>Todos los días envía un newsletter de parte de su sección especial de Print Magazine que se llama <a href="http://www.printmag.com/daily-heller/" target="_blank">‘The Daily Heller’</a> de la cual se tienen que inscribir en este momento si les interesa saber más sobre las raíces históricas del diseño gráfico mundial.</p>
-
-						<p>Steven Heller es uno de los mejores escritores dedicados al diseño gráfico que conozco, su aportación al conocimiento general que rodea la gráfica global es incomparable y punto de referencia para millones de diseñadores, incluyéndome.</p>
-					</div>
-					<div class="slim right">
-						<a herf="http://cronologia.mx/wp-content/uploads/2015/11/Screen-Shot-2015-06-05-at-10.20.43.png" target="_blank">
-							<img src="http://cronologia.mx/wp-content/uploads/2015/11/Screen-Shot-2015-06-05-at-10.20.43-300x169.png" alt="pulpo rosa en fondo azul">
-						</a>
-						<p class="caption">Se me hace importante destacar que nada hacia el noroeste.</p>
-
-					</div>
-				</article>
-
-			</div>
-		</div>
-
-
-
-	</section>
-
-
-
-
-	<section class="year">
-		<div class="period"><span>2010</span></div>
-		<hr>
-		<div class="contentContainer">
-			<ul class="articles">
-				<li class="article">
-					<a href="http://cronologia.mx/las-orquestas-y-el-jazz/" class="sofia">
-						Las Orquestas y el Jazz <em class="kepler">por Gerardo Ortíz</em>
-					</a>
-				</li>
-				<li class="article">
-					<a href="http://cronologia.mx/el-manifiesto-supermodernista/" class="sofia">
-						Supermodernismo <em class="kepler">por Mircea Turcan</em>
-					</a>
-				</li>
-			</ul>
-			<ul class="projects">
-				<li>
-					<a href="http://cronologia.mx/mitosis-010/" class="sofia">Mitosis 010</a>
-					<a href="../#comments" class="comments">2</a>
-				</li>
-				<li>
-					<a href="http://cronologia.mx/kinky/" class="sofia">Kinky</a>
-					<a href="../#comments" class="comments">1</a>
-				</li>
-				<li>
-					<a href="http://cronologia.mx/post-mas-nuevisimo/" class="sofia">Post más nuevísimo</a>
-					<a href="../#comments" class="no comments"></a>
-				</li>
-			</ul>
-		</div>
-	</section>
-	<section class="year">
-		<div class="period"><span>1988</span></div>
-		<hr>
-		<div class="contentContainer">
-			<ul class="projects">
-				<li>
-					<a href="http://cronologia.mx/escribiendo-a-cerca-de-los-pulpos/" class="sofia">
-						Escribiendo a cerca de los pulpos
-					</a>
-					<a href="../#comments" class="comments">1</a>
-				</li>
-			</ul>
-		</div>
-	</section>
-	<section class="year">
-		<div class="period"><span>1996</span></div>
-		<hr>
-		<div class="contentContainer">
-			<ul class="projects">
-				<li>
-					<a href="http://cronologia.mx/chuntaros-radio-poder/" class="sofia">1996: Chúntaros Radio Poder</a>
-					<a href="../#comments" class="comments">32</a>
-				</li>
-				<li>
-					<a href="http://cronologia.mx/grammys-96/" class="sofia">1996: Grammys 96</a>
-					<a href="../#comments" class="comments">1</a>
-				</li>
-			</ul>
-		</div>
-	</section>
-	<section class="year">
-		<div class="period"><span>2001</span></div>
-		<hr>
-		<div class="contentContainer">
-			<ul class="projects">
-				<li>
-					<a href="http://cronologia.mx/kinky/" class="sofia">2001: Kinky</a>
-					<a href="../#comments" class="no comments"></a>
-				</li>
-				<li>
-					<a href="http://cronologia.mx/post-mas-nuevisimo/" class="sofia">2015: Post más nuevísimo</a>
-					<a href="../#comments" class="comments">1</a>
-				</li>
-			</ul>
-		</div>
-	</section>
-</div><?php
+	endif;
 
 	get_footer(); ?>
---
+++ sultana/page-inicio.php
@@ -0,0 +1,220 @@
+<?php
+
+	/*
+	 * Template Name: Inicio
+	 */
+
+	get_header(); ?>
+
+	<section id="mainTitle">
+
+		<div id="Francesca" class="detailLine top"></div>
+
+		<div class="contain">
+			<div class="slim left">
+				<h1 class="logo">
+					Cronología
+					<span>del</span>
+					diseño gráfico
+					<span>en</span>
+					Monterrey
+				</h1>
+			</div>
+			<div id="timelineAbout" class="slim left">
+				<p>Un recuento y repositorio de la historia de la ciudad, su cultura y sus empresas a través del diseño gráfico.</p>
+
+				<p>Estás consultando el primer metalíbro por Raidho y curado Oscar Estrada.</p>
+			</div>
+		</div>
+
+	</section>
+
+
+
+
+
+
+
+	<section id="prologue">
+
+		<div class="detailLine"></div>
+
+		<div class="contain">
+			<div class="smallLogo">
+				O I O
+			</div>
+			<div class="slim left sofia">
+				1902 – 2015
+			</div>
+			<div class="wide right sofia" style="margin-bottom:6em">
+				I. <strong>Prólogo</strong>
+			</div>
+
+			<article>
+				<div class="slim left image">
+					<a herf="http://cronologia.mx/wp-content/uploads/2015/11/Screen-Shot-2015-06-05-at-10.20.43.png" target="_blank">
+						<img src="http://cronologia.mx/wp-content/uploads/2015/11/Screen-Shot-2015-06-05-at-10.20.43-300x169.png" alt="pulpo rosa en fondo azul">
+					</a>
+					<p class="caption">Se me hace importante destacar que nada hacia el noroeste.</p>
+				</div>
+
+				<div class="wide right">
+					<h2>Antigua Gráfica</h2>
+
+					<p>Steven Heller es uno de los mejores escritores dedicados al diseño gráfico que conozco, su aportación al conocimiento general que rodea la gráfica global es incomparable y punto de referencia para millones de diseñadores, incluyéndome.</p>
+
+					<p>Todos los días envía un newsletter de parte de su sección especial de Print Magazine que se llama <a href="http://www.printmag.com/daily-heller/" target="_blank">‘The Daily Heller’</a> de la cual se tienen que inscribir en este momento si les interesa saber más sobre las raíces históricas del diseño gráfico mundial.</p>
+				</div>
+			</article>
+		</div>
+
+	</section>
+
+
+
+
+
+
+
+<div id="timeline">
+
+
+	<section id="latestPost">
+		<div class="newPostLine"><span>Nuevo</span></div>
+
+		<div class="contain">
+			<a href="http://cronologia.mx/los-pioneros/" class="sofia project">
+				<h3>Los Pioneros</h3>
+				<p class="kepler">1900 por <span>Autor Apellido</span></p>
+			</a>
+		</div>
+	</section>
+
+
+
+
+	<section id="newChapter">
+
+		<div class="period range">
+			<span>1988<br>-<br>2001</span>
+			<button class="close">×</button>
+		</div>
+		<hr>
+		<div class="title">
+			<div class="contain">
+				<div class="wide left sofia">
+					<h2>Epígonos</h2>
+					<p class="author">Por Oscar Estrada</p>
+				</div>
+				<div class="slim right sofia">
+					<p>Poco antes de 1900 a 1950.</p>
+				</div>
+
+				<article class="show">
+					<div class="wide left">
+						<p>Steven Heller es uno de los mejores escritores dedicados al diseño gráfico que conozco, su aportación al conocimiento general que rodea la gráfica global es incomparable y punto de referencia para millones de diseñadores, incluyéndome.</p>
+
+						<p>Todos los días envía un newsletter de parte de su sección especial de Print Magazine que se llama <a href="http://www.printmag.com/daily-heller/" target="_blank">‘The Daily Heller’</a> de la cual se tienen que inscribir en este momento si les interesa saber más sobre las raíces históricas del diseño gráfico mundial.</p>
+
+						<p>Steven Heller es uno de los mejores escritores dedicados al diseño gráfico que conozco, su aportación al conocimiento general que rodea la gráfica global es incomparable y punto de referencia para millones de diseñadores, incluyéndome.</p>
+					</div>
+					<div class="slim right">
+						<a herf="http://cronologia.mx/wp-content/uploads/2015/11/Screen-Shot-2015-06-05-at-10.20.43.png" target="_blank">
+							<img src="http://cronologia.mx/wp-content/uploads/2015/11/Screen-Shot-2015-06-05-at-10.20.43-300x169.png" alt="pulpo rosa en fondo azul">
+						</a>
+						<p class="caption">Se me hace importante destacar que nada hacia el noroeste.</p>
+
+					</div>
+				</article>
+
+			</div>
+		</div>
+
+
+
+	</section>
+
+
+
+
+	<section class="year">
+		<div class="period"><span>2010</span></div>
+		<hr>
+		<div class="contentContainer">
+			<ul class="articles">
+				<li class="article">
+					<a href="http://cronologia.mx/las-orquestas-y-el-jazz/" class="sofia">
+						Las Orquestas y el Jazz <em class="kepler">por Gerardo Ortíz</em>
+					</a>
+				</li>
+				<li class="article">
+					<a href="http://cronologia.mx/el-manifiesto-supermodernista/" class="sofia">
+						Supermodernismo <em class="kepler">por Mircea Turcan</em>
+					</a>
+				</li>
+			</ul>
+			<ul class="projects">
+				<li>
+					<a href="http://cronologia.mx/mitosis-010/" class="sofia">Mitosis 010</a>
+					<a href="../#comments" class="comments">2</a>
+				</li>
+				<li>
+					<a href="http://cronologia.mx/kinky/" class="sofia">Kinky</a>
+					<a href="../#comments" class="comments">1</a>
+				</li>
+				<li>
+					<a href="http://cronologia.mx/post-mas-nuevisimo/" class="sofia">Post más nuevísimo</a>
+					<a href="../#comments" class="no comments"></a>
+				</li>
+			</ul>
+		</div>
+	</section>
+	<section class="year">
+		<div class="period"><span>1988</span></div>
+		<hr>
+		<div class="contentContainer">
+			<ul class="projects">
+				<li>
+					<a href="http://cronologia.mx/escribiendo-a-cerca-de-los-pulpos/" class="sofia">
+						Escribiendo a cerca de los pulpos
+					</a>
+					<a href="../#comments" class="comments">1</a>
+				</li>
+			</ul>
+		</div>
+	</section>
+	<section class="year">
+		<div class="period"><span>1996</span></div>
+		<hr>
+		<div class="contentContainer">
+			<ul class="projects">
+				<li>
+					<a href="http://cronologia.mx/chuntaros-radio-poder/" class="sofia">1996: Chúntaros Radio Poder</a>
+					<a href="../#comments" class="comments">32</a>
+				</li>
+				<li>
+					<a href="http://cronologia.mx/grammys-96/" class="sofia">1996: Grammys 96</a>
+					<a href="../#comments" class="comments">1</a>
+				</li>
+			</ul>
+		</div>
+	</section>
+	<section class="year">
+		<div class="period"><span>2001</span></div>
+		<hr>
+		<div class="contentContainer">
+			<ul class="projects">
+				<li>
+					<a href="http://cronologia.mx/kinky/" class="sofia">2001: Kinky</a>
+					<a href="../#comments" class="no comments"></a>
+				</li>
+				<li>
+					<a href="http://cronologia.mx/post-mas-nuevisimo/" class="sofia">2015: Post más nuevísimo</a>
+					<a href="../#comments" class="comments">1</a>
+				</li>
+			</ul>
+		</div>
+	</section>
+</div><?php
+
+	get_footer(); ?>
