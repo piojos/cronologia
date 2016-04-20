ab8b2a1777283bbb4b6fd1732d3ad9cc788e5f64
raidho-mx <miranda@raidho.mx>
2016-04-19 19:40:50 -0500

    Working timeline with chapters

--- sultana/page-inicio.php
+++ sultana/page-inicio.php
@@ -17,6 +17,7 @@
 		'order'				=> 'ASC'
 	));
 
+	endwhile; ?>
 
 
 
@@ -26,35 +27,6 @@
 
 
 
-	/*  INTRO  */
-
-	$introAbout = get_field('intro_about');
-
-	?>
-	<section id="mainTitle">
-
-		<div id="Francesca" class="detailLine top"></div>
-
-		<div class="contain">
-			<div class="slim left">
-				<h1 class="logo">
-					Cronología
-					<span>del</span>
-					diseño gráfico
-					<span>en</span>
-					Monterrey
-				</h1>
-			</div><?php
-
-		if($introAbout) { ?>
-			<div id="timelineAbout" class="slim left">
-				<?php echo $introAbout; ?>
-			</div><?php
-		} ?>
-
-		</div>
-
-	</section><?php
 
 
 
@@ -62,48 +34,6 @@
 
 
 
-
-	/*  PROLOGUE  */
-
-	$prologueToggle = get_field('has_prologue');
-	$prologueQuery = new WP_Query( 'post_type=page&pagename=prologo' );
-
-	if($prologueToggle) :
-		while ( $prologueQuery->have_posts() ) :
-			$prologueQuery->the_post(); ?>
-		<section id="prologue">
-
-			<div class="detailLine"></div>
-
-			<div class="contain">
-				<div class="smallLogo">
-					O I O
-				</div><?php
-
-				$ftdCaption = get_post(get_post_thumbnail_id())->post_excerpt;
-				echo '<div class="slim left sofia">'.$ftdCaption.'</div>'; ?>
-
-				<div class="wide right sofia" style="margin-bottom:6em"><?php
-					if($rTitle) {
-						echo $rTitle;
-					} else {
-						the_title();
-					} ?>
-				</div>
-
-				<article><?php
-
-					get_template_part('inc/block-content'); ?>
-
-				</article>
-			</div>
-
-		</section><?php
-		endwhile;
-		wp_reset_postdata();
-	endif;
-
-endwhile; // PAGE: Inicio Loop ?>
 
 
 
@@ -116,54 +46,118 @@ endwhile; // PAGE: Inicio Loop ?>
 
 
 <div id="timeline"><?php
-		if( $posts ):
-
-			foreach( $posts as $post ):
-				setup_postdata( $post );
+	if( $posts ):
+		foreach( $posts as $post ):
+			setup_postdata( $post );
 
 			$annoNuevo = get_field('year');
 
-				if($anno == '') { // Post más reciente.
-					if (in_category('2')) { // Es Artículo
-						echo '<section class="year latest"><div class="number">Artículo</div><ul class="projects">';
-					} else { // Es Post
-						echo '<section class="year latest"><div class="number">Nuevo</div><ul class="projects">';
-					}
-					// echo '<section class="year latest"><div class="number">Latest</div><ul class="projects">';
-				} elseif($annoNuevo == $anno) {} else { ?>
-					</section>
-					<section class="year">
-						<div class="period"><span><?php echo $annoNuevo; ?></span></div>
-						<hr>
-						<div class="contentContainer">
-						<ul class="projects"><?php
+
+// NEW YEAR
+			if($annoNuevo == $anno) {} else {
+
+
+				if($anno != '') {
+					if($isChapter) {
+					$isChapter = FALSE; ?>
+		</div><?php
+					} else { ?>
+			</ul>
+		</div><?php
+					} ?>
+
+	</section><?php
 				}
 
-				$cat = get_the_category(); ?>
 
+				if(in_category('7')) { ?>
+
+	<section id="newChapter">
+		<div class="period range">
+			<span><?php echo $annoNuevo ?><br>-<br>N/A</span>
+			<button class="close">×</button>
+		</div>
+		<hr>
+		<div class="title"><?php
+
+				} else { ?>
+
+	<section class="year">
+		<div class="period">
+			<span><?php echo $annoNuevo ?></span>
+		</div>
+		<hr>
+		<div class="contentContainer">
+			<ul class="articles"></ul>
+			<ul class="projects"><?php
+				}
+
+
+
+			} // END if new year
+
+
+
+// Actual LOOP
+			if(in_category('7')) { ?>
+
+			<div class="contain <?php if($annoNuevo == $anno) echo 'trappedChapter'; ?>">
+				<div class="wide left sofia">
+					<h2><?php the_title(); ?></h2>
+					<p class="author">Por Oscar Estrada</p>
+				</div>
+				<div class="slim right sofia">
+					<p>Poco antes de 1900 a 1950.</p>
+				</div>
+
+				<article class="show">
+					<div class="wide left">
+						<p>Steven Heller es uno de los mejores escritores dedicados al diseño gráfico que conozco, su aportación al conocimiento general que rodea la gráfica global es incomparable y punto de referencia para millones de diseñadores, incluyéndome.</p>
+
+						<p>Todos los días envía un newsletter de parte de su sección especial de Print Magazine que se llama <a href="http://www.printmag.com/daily-heller/" target="_blank">‘The Daily Heller’</a> de la cual se tienen que inscribir en este momento si les interesa saber más sobre las raíces históricas del diseño gráfico mundial.</p>
+
+						<p>Steven Heller es uno de los mejores escritores dedicados al diseño gráfico que conozco, su aportación al conocimiento general que rodea la gráfica global es incomparable y punto de referencia para millones de diseñadores, incluyéndome.</p>
+					</div>
+					<div class="slim right">
+						<a herf="http://cronologia.mx/wp-content/uploads/2015/11/Screen-Shot-2015-06-05-at-10.20.43.png" target="_blank">
+							<img src="http://cronologia.mx/wp-content/uploads/2015/11/Screen-Shot-2015-06-05-at-10.20.43-300x169.png" alt="pulpo rosa en fondo azul">
+						</a>
+						<p class="caption">Se me hace importante destacar que nada hacia el noroeste.</p>
+
+					</div>
+				</article>
+			</div>
+<?php
+				if($annoNuevo == $anno) {
+				}
+				$isChapter = TRUE;
+			} elseif(($annoNuevo != $anno) OR ($annoNuevo == $anno)) { ?>
 
 				<li<?php if (in_category('2')) echo ' class="article"'; ?>>
-					<a href="<?php the_permalink(); ?>" class="sofia">
-						<?php the_title();
+					<a href="<?php the_permalink(); ?>" class="sofia"><?php
+						if(in_category('7')) {
+							echo '🍕: ';
+						}
+						the_title();
 						if (in_category('2')) {
 							echo ' <em class="kepler">por '.get_the_author().'</em>';
 						} ?></a>
-				</li><?php
+				</li>
+<?php
+			}
 
 
 			$anno = get_field('year');
 
-				if($anno == '') {
-					echo '</ul></section>';
-				} elseif($annoNuevo == $anno) {} else {
-					echo '</ul></section>';
-				}
+		endforeach;
+		wp_reset_postdata();
 
-			endforeach;
-			wp_reset_postdata();
+	endif; ?>
+			</ul>
+		</div>
+	</section>
+</div>
 
-		endif; ?>
-
-</div><?php // END #timeline
+<?php // END #timeline
 
 	get_footer(); ?>
